f90533be3de6e2be63faec8927c038498d225c52
Brandt Walton <brandtdwalton@gmail.com>
2021-04-07 00:38:59 -0700

    I modified scalper.py to workgit status

--- scalper.py
+++ scalper.py
@@ -1,19 +1,30 @@
 from selenium import webdriver
 import time
+import threading
 
 browser = webdriver.Chrome("C:/Users/brand/OneDrive/Documents/Coding/Bestbuy/chromedriver.exe")
 
-browser.get("https://www.bestbuy.com/site/apple-watch-series-6-gps-44mm-space-gray-aluminum-case-with-black-sport-band-space-gray/6215931.p?skuId=6215931")
+def my_function(address):
+    browser.get(address)
+    buyButton = False
 
-buyButton = False
-
-while not buyButton:
-        try:
-            addtocartbtn = browser.find_element_by_class_name("btn-disabled")
-            time.sleep(1)
-            browser.refresh()
-        except:
+    while not buyButton:
             try:
-                browser.find_element_by_xpath("//button[contains(text(), 'Add to Cart')]").click()
+                #addtocartbtn = browser.find_element_by_class_name("btn-disabled")
+                addtocartbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]")
+                time.sleep(1)
+                browser.refresh()
             except:
-                buyButton = True
+                try:
+                    browser.find_element_by_class_name("add-to-cart-button").click()
+                    time.sleep(1)
+                    browser.find_element_by_xpath("/html/body/div[8]/div/div[1]/div/div/div/div/div[1]/div[3]/a").click()
+                    time.sleep(1)
+                    #browser.find_element_by_xpath("/html/body/div[1]/main/div/div[2]/div[1]/div/div[1]/div[1]/section[2]/div/div/div[3]/div/div[1]/button").click()
+                    browser.find_element_by_class_name("btn-primary").click()
+
+                except:
+                    buyButton = True
+
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")
+my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
