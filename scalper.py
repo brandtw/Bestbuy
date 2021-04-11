7eaf5ad9bf4c20d58959ff7510519597ee99181d
chaseduncs50 <chasedun@gmail.com>
2021-04-11 00:34:11 -0700

    See last commit for more comments... broke stuff, added more documenation

--- scalper.py
+++ scalper.py
@@ -1,4 +1,4 @@
-# To do: beeper, threading, continuously click "please wait" IF it appears, address comments below
+# To do: beeper, threading, continuously click "please wait" IF it appears, fix comments below, during threading maybe have multiple accounts?
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,7 +21,7 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname,'chromedriver.exe')
 browser = webdriver.Chrome(filename)
 soundname = os.path.join(dirname,'giveyouup.mp3')       # We should thread this sound when the bot clicks "Add to Cart" to notify the user that the item is now in stock.
-wait = WebDriverWait(browser,25)                        # <-- Set webpage timeout to max 25 seconds                                                                     
+wait = WebDriverWait(browser,30)                        # <-- Set webpage timeout to max 25 seconds                                                                     
 
 # Main Function
 def my_function(address):
@@ -32,9 +32,9 @@ def my_function(address):
     buyButton = False
 
     # Auto Login (NOT CURRENTLY WORKING. Will not click sign in button.)
-    time.sleep(8)
+    time.sleep(6)
     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    browser.find_element_by_xpath('//*[@id="large-footer"]/div[2]/div[1]/div[1]/ul/li[3]').click            # I CANNOT FIGURE OUT HOW TO HIT THE SIGN IN BUTTON FOR THE LIFE OF ME
+    browser.find_element_by_partial_link_text('signin').click            # I CANNOT FIGURE OUT HOW TO HIT THE SIGN IN BUTTON FOR THE LIFE OF ME
     wait.until(EC.presence_of_element_located((By.ID,"fld-e")))
     browser.find_element_by_id("fld-e").send_keys(username)
     browser.find_element_by_id("fld-p1").send_keys(password)
@@ -43,8 +43,9 @@ def my_function(address):
 
     # Item Selection + Cart + Checkout Automation
     while not buyButton:        
-        try:
-            outofstockbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]") # Research way to keep code from waiting for website to load?
+        try:                 
+            outofstockbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]") # THIS IS BROKEN!!!!!! Research way to keep code from waiting for website to load?
+            time.sleep(1)                                                                               # When this gets fixed, we should remove this function.
             browser.refresh()
         except:
             try:                                                        
@@ -56,15 +57,31 @@ def my_function(address):
             except:
                 buyButton = True
 
-print("PROGRAM TERMINATED\n")
+
+# Threading Objects
+#t1 = threading.Thread(target=gpu3060ti)
+# t2 = gpu3070()
+# g3 = gpu3080()
+# g4 = gpu3090()
+
+# t1.start()
+# g2.start()
+# g3.start()
+# g4.start()
+
+# t1.join()
 
 
 # GPU http Links
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
-#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
+my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")       # 3090
 
 # Test http Links
 #my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
-my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
+#my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
+
+
+# Survey Invite button ID = survey_invite_no <--------------- INSERT IF FUNCTION TO CLICK NO HERE?
+# Survey Invite border ID = survey_border
