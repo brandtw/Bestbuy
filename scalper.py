6a128f83c5ee3edefb61a7218b12e96114b58308
chaseduncs50 <chasedun@gmail.com>
2021-04-10 22:19:31 -0700

    Sounds prototyping, auto login moved to beginning of sequence (needs fixing), checkout automation condensed, added detailed documentation

--- scalper.py
+++ scalper.py
@@ -1,59 +1,70 @@
-# To do: beeper, threading, continuously click "please wait" IF it appears, quickest possible way to get to the checkout button
-# Add logic so the user has to input the password twice and the program checks to see if the passwords match.
+# To do: beeper, threading, continuously click "please wait" IF it appears, address comments below
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from playsound import playsound
 import pyautogui
 import os
 import time
 import threading
 
-# Username and Password input
-print(" ")
-print("!!!!!!!!!!!!!!!!! MAKE SURE YOUR INFORMATION IS 100% CORRECT. DOUBLE CHECK. !!!!!!!!!!!!!!!!!")
+# Username and Password User Input
+print("\n!!!!!!!!!!!!!!!!! MAKE SURE YOUR INFORMATION IS 100% CORRECT. DOUBLE CHECK. !!!!!!!!!!!!!!!!!")
 username = input("Username: ")
 password = input("Password: ")
 
-# Browser Launch
+# Main Prototyping
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname,'chromedriver.exe')
 browser = webdriver.Chrome(filename)
+soundname = os.path.join(dirname,'giveyouup.mp3')       # We should thread this sound when the bot clicks "Add to Cart" to notify the user that the item is now in stock.
+wait = WebDriverWait(browser,25)                        # <-- Set webpage timeout to max 25 seconds                                                                     
 
 # Main Function
 def my_function(address):
+    
+    # Browser Launch Sequence + Full Screen
     browser.get(address)
+    pyautogui.hotkey('winleft', 'up')
     buyButton = False
 
-    while not buyButton:
-            try:
-                # addtocartbtn = browser.find_element_by_class_name("btn-disabled")
-                addtocartbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]")  # Research way to keep code from waiting for website to load?
-                time.sleep(1)
-                browser.refresh()
+    # Auto Login (NOT CURRENTLY WORKING. Will not click sign in button.)
+    time.sleep(8)
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    browser.find_element_by_xpath('//*[@id="large-footer"]/div[2]/div[1]/div[1]/ul/li[3]').click            # I CANNOT FIGURE OUT HOW TO HIT THE SIGN IN BUTTON FOR THE LIFE OF ME
+    wait.until(EC.presence_of_element_located((By.ID,"fld-e")))
+    browser.find_element_by_id("fld-e").send_keys(username)
+    browser.find_element_by_id("fld-p1").send_keys(password)
+    browser.find_element_by_class_name("c-checkbox-custom-input").click
+    browser.find_element_by_xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[4]/button").click()
+
+    # Item Selection + Cart + Checkout Automation
+    while not buyButton:        
+        try:
+            outofstockbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]") # Research way to keep code from waiting for website to load?
+            browser.refresh()
+        except:
+            try:                                                        
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME,"add-to-cart-button")))        # Sleep until button is clickable -- Here is where we might be able to have the computer wait for the "please wait" to convert into a clickable button. Speak with twins
+                browser.find_element_by_class_name("add-to-cart-button").click()                        # ADD TO CART BUTTON CLICK. We should thread in a sound here to notify user.
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME,"spinner-sm"))) == None        # Checks to see if 'add to cart' action is still loading. Does this work?
+                browser.find_element_by_class_name("shop-cart-icon").click()                            # Consider adding: if NOT located at 'https://www.bestbuy.com/cart', click shop cart icon again and wait. Else, click checkout button...
+                browser.find_element_by_class_name("btn-primary").click()                               
             except:
-                try:
-                    wait=WebDriverWait(browser,15)                                                          # Timeout max 15 seconds
-                    wait.until(EC.presence_of_element_located((By.CLASS_NAME,"add-to-cart-button")))        # Sleep until button is clickable -- Here is where we might be able to have the computer wait for the "please wait" to convert into a clickable button. Speak with twins
-                    browser.find_element_by_class_name("add-to-cart-button").click()                        # ADD TO CART BUTTON CLICK
-                    browser.find_element_by_class_name("shop-cart-icon").click()                            # if NOT located at 'https://www.bestbuy.com/cart', click shop cart icon again and wait. Else, click checkout button...
-                    browser.find_element_by_class_name("btn-primary").click()                               # CHECKOUT BUTTON CLICK
+                buyButton = True
 
-                    # Auto-Login
-                    wait.until(EC.presence_of_element_located((By.ID,"fld-p1")))
-                    username_field = browser.find_element_by_id("fld-e")
-                    username_field.send_keys(username)
-                    password_field = browser.find_element_by_id("fld-p1")
-                    password_field.send_keys(password)
-                    browser.find_element_by_xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[3]/button").click()
-
-                except:
-                    buyButton = True
+print("PROGRAM TERMINATED\n")
 
 
-# GPU http Links:
-#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")
+# GPU http Links
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")       # 3090
+
+# Test http Links
 #my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
 my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
