a53df8830895e54d7fb59c54bd559a5578468147
chaseduncs50 <chasedun@gmail.com>
2021-04-11 15:05:08 -0700

    Fixed bugs, other changes

--- scalper.py
+++ scalper.py
@@ -21,7 +21,7 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname,'chromedriver.exe')
 browser = webdriver.Chrome(filename)
 soundname = os.path.join(dirname,'giveyouup.mp3')       # We should thread this sound when the bot clicks "Add to Cart" to notify the user that the item is now in stock.
-wait = WebDriverWait(browser,30)                        # <-- Set webpage timeout to max 25 seconds                                                                     
+wait = WebDriverWait(browser,40)                        # Webpage timeout set to max 40 seconds                                                                     
 
 # Main Function
 def my_function(address):
@@ -31,26 +31,27 @@ def my_function(address):
     pyautogui.hotkey('winleft', 'up')
     buyButton = False
 
-    # Auto Login (NOT CURRENTLY WORKING. Will not click sign in button.)
-    time.sleep(12)
+    # Auto Login (FIXED)
+    time.sleep(3)
     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    # browser.find_element_by_partial_link_text('signin').click            # I CANNOT FIGURE OUT HOW TO HIT THE SIGN IN BUTTON FOR THE LIFE OF ME
+    browser.find_element_by_xpath('//*[@id="large-footer"]/div[2]/div[1]/div[1]/ul/li[3]').click()
     wait.until(EC.presence_of_element_located((By.ID,"fld-e")))
     browser.find_element_by_id("fld-e").send_keys(username)
     browser.find_element_by_id("fld-p1").send_keys(password)
-    browser.find_element_by_class_name("c-checkbox-custom-input").click
+    browser.find_element_by_class_name("c-checkbox-custom-input").click()
     browser.find_element_by_xpath("/html/body/div[1]/div/section/main/div[2]/div[1]/div/div/div/div/form/div[4]/button").click()
 
     # Item Selection + Cart + Checkout Automation
     while not buyButton:        
         try:                 
-            outofstockbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]") # This works, but there is a bug when you sign in first. Then it won't refresh the page for some reason. I even tried pyautogui.hotkey('f5')
+            outofstockbtn = browser.find_element_by_xpath("//button[contains(@class, 'btn-disabled')]") #._execute(Command.IS_ELEMENT_ENABLED)[False] # This works, but there is a bug when you sign in first. Then it won't refresh the page for some reason. I even tried pyautogui.hotkey('f5')
             browser.refresh()
         except:
             try:                                                        
-                wait.until(EC.presence_of_element_located((By.CLASS_NAME,"add-to-cart-button")))        # Sleep until button is clickable -- Here is where we might be able to have the computer wait for the "please wait" to convert into a clickable button. Speak with twins
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME,"add-to-cart-button")))        # Sleep until button is clickable
                 browser.find_element_by_class_name("add-to-cart-button").click()                        # ADD TO CART BUTTON CLICK. We should thread in a sound here to notify user.
                 wait.until(EC.presence_of_element_located((By.CLASS_NAME,"spinner-sm"))) == None        # Checks to see if 'add to cart' action is still loading. Does this work?
+                #Here is where we might be able to have the computer wait for the "please wait" to convert into a clickable button. Speak with twins.
                 browser.find_element_by_class_name("shop-cart-icon").click()                            # Consider adding: if NOT located at 'https://www.bestbuy.com/cart', click shop cart icon again and wait. Else, click checkout button...
                 browser.find_element_by_class_name("btn-primary").click()                               
             except:
@@ -73,13 +74,13 @@ def my_function(address):
 
 # GPU http Links
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
-my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
+# my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
 # my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")       # 3090
 
 # Test http Links
 #my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
-#my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
+my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
 
 
 # Survey Invite button ID = survey_invite_no <--------------- INSERT IF FUNCTION TO CLICK NO HERE?
