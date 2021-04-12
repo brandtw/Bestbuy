8da1d7e5d048ce1a1a4a5bad7fed7518f1a0fba6
chaseduncs50 <chasedun@gmail.com>
2021-04-11 23:48:37 -0700

    Chase is a master coder

--- scalper.py
+++ scalper.py
@@ -1,4 +1,4 @@
-# To do: beeper, threading, continuously click "please wait" IF it appears, fix comments below, during threading maybe have multiple accounts?
+# To do: beeper, threading, consider IF function for survey, fix comments below, during threading maybe have multiple accounts?
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -20,7 +20,7 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname,'chromedriver.exe')
 browser = webdriver.Chrome(filename)
 soundname = os.path.join(dirname,'giveyouup.mp3')       # We should thread this sound when the bot clicks "Add to Cart" to notify the user that the item is now in stock.
-wait = WebDriverWait(browser,60)                        # Webpage timeout set to max 60 seconds                                                                     
+wait = WebDriverWait(browser,6000)                      # Webpage timeout set to max 6000 seconds                                                                     
 
 # Main Function
 def my_function(address):
@@ -30,7 +30,7 @@ def my_function(address):
     browser.maximize_window()
     buyButton = False
 
-    # Auto Login (FIXED)
+    # Auto Login
     time.sleep(3)
     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     browser.find_element_by_xpath('//*[@id="large-footer"]/div[2]/div[1]/div[1]/ul/li[3]').click()
@@ -43,7 +43,7 @@ def my_function(address):
     # Item Selection + Cart + Checkout Automation
     while not buyButton:        
         try:
-            wait.until(EC.presence_of_element_located((By.XPATH,'/html/body/div[3]/main/div[2]/div[3]/div[2]')))
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME,'fulfillment-add-to-cart-button')))    # BRANDT, I had to change this. Talk to me before editing.
             browser.find_element_by_class_name("btn-disabled")
             browser.refresh()            
         except:
@@ -51,18 +51,17 @@ def my_function(address):
                 try:
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME,"add-to-cart-button")))        # Sleep until button is clickable
                     browser.find_element_by_class_name("add-to-cart-button").click()                        # ADD TO CART BUTTON CLICK. We should thread in a sound here to notify user.
-                    EC.presence_of_element_located((By.CLASS_NAME,'added-to-cart'))
-                    browser.find_element_by_class_name("shop-cart-icon").click()                            # Consider adding: if NOT located at 'https://www.bestbuy.com/cart', click shop cart icon again and wait. Else, click checkout button...
+                    print("x")
+                    time.sleep(5)                                                                           # WE NEED TO REPLACE THIS WITH SOMETHING FASTER/MORE RELIABLE
+                    browser.find_element_by_class_name("added-to-cart")                                     # Archive: Previous -- EC.presence_of_element_located((By.CLASS_NAME,'added-to-cart'))
+                    print("Success!!!")
+                    browser.find_element_by_class_name("go-to-cart-button").click()
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME,"checkout-buttons")))
-                    browser.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button').click()
+                    browser.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button').click() # Chase: Consider changing
                     buyButton = True
-                #Here is where we might be able to have the computer wait for the "please wait" to convert into a clickable button. Speak with twins.
                 except:
                     pass
 
-
-
-# class = added-to-cart
 
 # Threading Objects
 # t1 = threading.Thread(target=gpu3060ti)
@@ -81,12 +80,12 @@ def my_function(address):
 # GPU http Links
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
 # my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
-# my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
+my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")       # 3090
 
 # Test http Links
 #my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
-my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
+# my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
 
 
 # Survey Invite button ID = survey_invite_no <--------------- INSERT IF FUNCTION TO CLICK NO HERE?
