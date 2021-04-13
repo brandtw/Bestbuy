264f1e05e2a40c178607df0ea652a129f104bdec
Brandt Walton <brandtdwalton@gmail.com>
2021-04-13 01:04:57 -0700

    FINAL YEET

--- scalper.py
+++ scalper.py
@@ -1,5 +1,3 @@
-# To do: threading, consider IF function for survey, fix comments below, during threading maybe have multiple accounts?
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,8 +6,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from playsound import playsound 
 import os
 import time
-import threading
-import datetime
 
 # Username and Password User Input
 print("\n!!!!!!!!!!!!!!!!! MAKE SURE YOUR INFORMATION IS 100% CORRECT. DOUBLE CHECK. !!!!!!!!!!!!!!!!!")
@@ -44,47 +40,35 @@ def my_function(address):
     # Item Selection + Cart + Checkout Automation
     while not buyButton:        
         try:
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME,'fulfillment-add-to-cart-button')))        # BRANDT, I had to change this. Talk to me before editing.
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME,'fulfillment-add-to-cart-button')))
             browser.find_element_by_class_name("btn-disabled")
             browser.refresh()            
         except:
+            playsound(RickRoll, block=False)
             while not buyButton:
                 try:
                     browser.find_element_by_xpath('//*[@id="shop-cart-icon-02c028e8-4638-43e6-96ff-96d1c70cfe72"]/div/div/div/a/div')
-                    #playsound(RickRoll, block=False)
-                    #wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="shop-cart-icon-02c028e8-4638-43e6-96ff-96d1c70cfe72"]/div/div/div/a/div'))) 
                     browser.find_element_by_class_name("shop-cart-icon").click()
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME,"checkout-buttons")))
-                    browser.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button').click() # Chase: Consider changing
+                    browser.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button').click()
                     buyButton = True
                 except:
                     browser.find_element_by_class_name("add-to-cart-button").click()
+    
+    time.sleep(300)
 
-
-# # Threading Objects
-# t1 = threading.Thread(target=my_function1)
-# t2 = threading.Thread(target=my_function2)
-# # g3 = gpu3080()
-# # g4 = gpu3090()
-
-# t1.start()
-# t2.start()
-# # g3.start()
-# # g4.start()
-
-# t1.join()
-# t2.join()
 
 # GPU http Links
-# my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
-# my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
-# my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
+
+my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")         # 3060 ti
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")    # 3070
+#my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")       # 3080
 #my_function("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")       # 3090
+
+
+
+
 
 # Test http Links
 #my_function("https://www.bestbuy.com/site/wd-blue-4tb-internal-sata-hard-drive-for-desktops/9026007.p?skuId=9026007")
-my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
-
-
-# Survey Invite button ID = survey_invite_no <--------------- INSERT IF FUNCTION TO CLICK NO HERE?
-# Survey Invite border ID = survey_border
+#my_function("https://www.bestbuy.com/site/apple-10-2-inch-ipad-latest-model-8th-generation-with-wi-fi-32gb-space-gray/5199701.p?skuId=5199701")
