dfcdbc844632d05dff44ea96b38a08188549e107
Brandt Walton <brandtdwalton@gmail.com>
2021-04-27 00:29:25 -0700

    working

--- bot.py
+++ bot.py
@@ -11,24 +11,31 @@ while flag == 0:
     flag2 = 0
     soldout = pyautogui.locateCenterOnScreen("soldout.png", confidence= 0.9)
     addtocart = pyautogui.locateCenterOnScreen("addtocart.png", confidence= 0.7)
+    gpu = pyautogui.locateCenterOnScreen("gpu.png", confidence= 0.9)
     if addtocart != None:
         playsound(alert, block=False)
         pyautogui.click(addtocart)
+        pyautogui.moveTo(200,80)
         time.sleep(1)
         flag = 1
-    elif soldout != None:
+    elif gpu != None:
         pyautogui.press('f5')
-        time.sleep(3)
+        time.sleep(1)
 
 while True:
-    addtocart = pyautogui.locateCenterOnScreen("addtocart.png", confidence= 0.7)
+    addtocart = pyautogui.locateCenterOnScreen("addtocart.png", confidence= 0.95)
     gotocart = pyautogui.locateCenterOnScreen("gotocart.png", confidence= 0.7)
     checkout = pyautogui.locateCenterOnScreen("checkout.png", confidence= 0.9)
-    if addtocart != None:
-        time.sleep(1)
-        pyautogui.click(addtocart)
     if gotocart != None:
         pyautogui.click(gotocart)
-    if checkout != None:
-        pyautogui.moveTo(80,80)
+        pyautogui.moveTo(200,80)
+    elif checkout != None:
         pyautogui.click(checkout)
+        pyautogui.moveTo(200,80)
+        time.sleep(1)
+        pyautogui.click(addtocart)
+        pyautogui.moveTo(200,80)
+    elif addtocart != None:
+        time.sleep(1)
+        pyautogui.click(addtocart)
+        pyautogui.moveTo(200,80)
